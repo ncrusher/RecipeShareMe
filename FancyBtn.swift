50dc7b1fb3b36f26464de35b92f03a5ed9142002
Khai Le <khaikhai@Khais-MBP.attlocal.net>
2017-04-26 22:13:22 -0700

    email-auth

--- FancyBtn.swift
+++ FancyBtn.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2017 Kaiba. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class FancyBtn: UIButton {
+    
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: SHADOW_GRAY).cgColor
+        layer.shadowOpacity = 4.0
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 2.0) // how far the shadow goes out
+        layer.cornerRadius = 2.0
+        
+    }
+    
+}
+
--- RecipeShareMe/ViewController.swift
+++ RecipeShareMe/ViewController.swift
@@ -7,18 +7,113 @@
 //
 
 import UIKit
+import Foundation
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+import Firebase
+import FacebookLogin
 
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate{
+
+    @IBOutlet weak var FBButton: FBSDKLoginButton!
+
+    @IBOutlet weak var emailField: UITextField!
+
+    @IBOutlet weak var passwordField: UITextField!
+
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        
+    FBButton.delegate = self
+    FBButton.readPermissions = ["email"]
+    
+        
+        
+    super.viewDidLoad()
+        
+        
+        
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    @IBAction func signinPressed(_ sender: Any) {
+        
+        if let email = emailField.text, let pwd = passwordField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil { //if no errors then we are sign in
+                    print("JESS: Email user authenticated with Firebase")
+                    
+                    
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("JESS: Unable to authenticate with Firebase using email")
+                            
+                        } else {
+                            print("JESS: Successfully authenticated with Firebase")
+                            
+                        }
+                    })
+                }
+            })
+        }
     }
+    
+    
+    func loginButtonDidLogOut(_ FFButton: FBSDKLoginButton!) {
+        
+        print("Did Logout out of facebook")
+    }
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        
+        if error != nil {
+            print(error)
+            return
+            
+        }
+        
+        showEmailAddress()
+
+        }
+    
+    
+    func showEmailAddress() {
+        
+        let accessToken = FBSDKAccessToken.current() // gets the accesstoken for you
+        guard let accessTokenString = accessToken?.tokenString else //guards against errors when getting the access token string
+        { return }
+        
+        let credentials = FIRFacebookAuthProvider.credential(withAccessToken: accessTokenString)
+        
+        FIRAuth.auth()?.signIn(with: credentials, completion: { (user, error) in
+            
+            if error != nil {
+
+                print("Something went wrong with our FB user: ", error!)
+                return
+            }
+            
+            print("Successfully logged in with our user: ", user!)
+        })
+        
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start { (connection, result, err) in
+            
+            if err != nil {
+                print("Failed to start graph request:", err!)
+                return
+            }
+            
+            print(result!)
+            
+            
+    }
+}
+    
 
 
 }
