5a87c8d8e0fd2ea74ba203870f27036ec184cf6e
Khai Le <khaikhai@Khais-MBP.attlocal.net>
2017-04-29 23:26:02 -0700

    database

---
+++ PostCell.swift
@@ -0,0 +1,31 @@
+//
+//  PostCell.swift
+//  RecipeShareMe
+//
+//  Created by Khai Le on 4/28/17.
+//  Copyright © 2017 Kaiba. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLbl:UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+        
+        
+    }
+    
+
+    
+
+}
+
+
