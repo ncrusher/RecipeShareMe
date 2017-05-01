b61686bae4e6a4413ce1804f2db5327bab64ea09
Khai Le <khaikhai@Khais-MBP.attlocal.net>
2017-05-01 00:31:14 -0700

    Finished parasing data

--- FeedVC.swift
+++ FeedVC.swift
@@ -14,6 +14,8 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,8 +27,23 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
             
-          print(snapshot.value)
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                
+                for snap in snapshot {
+                    
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, Any> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
+            
         })
+        
+        
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -36,12 +53,16 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 3
+
+        return posts.count
         
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let post = posts[indexPath.row]
+        print("JESS: \(post.caption)")
         
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
         
